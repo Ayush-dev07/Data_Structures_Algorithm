eb24e735c577278c9b8695bc4d6fe5fe6fa56450
Ayush Yadav <ayushyadav9812546@gmail.com>
2026-07-08 21:32:48 +0530

    Array Operations

--- Basics/Array_ADT.cpp
+++ Basics/Array_ADT.cpp
@@ -37,6 +37,58 @@ void insert(struct array *arr, int index, int x)
     }
 }
 
+int get(struct array arr, int index)
+{
+    if(index>0 && index<arr.length)
+        return arr.A[index];
+    return -1;
+}
+
+void set(struct array *arr, int index, int x)
+{
+    if(index>0 && index<arr->length)
+        arr->A[index]=x;
+}
+
+int max(struct array arr)
+{
+    int max=arr.A[0];
+    int i;
+    for (i=1;i<arr.length;i++)
+    {
+        if (arr.A[i]>max)
+            max=arr.A[i];
+    }
+    return max;
+}
+
+int min(struct array arr)
+{
+    int min=arr.A[0];
+    int i;
+    for (i=1;i<arr.length;i++)
+    {
+        if (arr.A[i]<min)
+            min=arr.A[i];
+    }
+    return min;
+}
+
+int sum(struct array arr)
+{
+    int s=0;
+    int i;
+    for (i=0;i<arr.length;i++)
+        s+=arr.A[i];
+        
+    return s;
+}
+
+float avg(struct array arr)
+{
+    return (float)sum(arr)/arr.length;
+}
+
 int main()
 {
     struct array arr={{2,3,4,5,6}, 20, 5};
