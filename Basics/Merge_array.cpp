5b218ba206156aa978b1baef156d53905d6625a7
Ayush Yadav <ayushyadav9812546@gmail.com>
2026-07-10 11:48:19 +0530

    Merge and sort two sorted arrays

---
+++ Basics/Merge_array.cpp
@@ -0,0 +1,54 @@
+//Merge and sort two sorted arrays
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct array
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+void display(struct array arr)
+{
+    int i;
+    printf("\nElements are:\n");
+    for (i=0;i<arr.length;i++)
+        printf("%d ",arr.A[i]);
+}
+
+
+struct array* merge(struct array *arr1, struct array *arr2)
+{
+    int i,j,k;
+    i=j=k=0;
+    
+    struct array *arr3=(struct array *)malloc(sizeof(struct array));
+    while (i<arr1->length && j<arr2->length)
+    {
+        if (arr1->A[i]<arr2->A[j])
+            arr3->A[k++]=arr1->A[i++];
+        else
+            arr3->A[k++]=arr2->A[j++];
+    }
+    for (;i<arr1->length;i++)
+        arr3->A[k++]=arr1->A[i];
+    for (;j<arr2->length;j++)
+        arr3->A[k++]=arr2->A[j];
+    arr3->length=arr1->length+arr2->length;
+    arr3->size=10;
+    
+    return arr3;
+}
+
+int main()
+{
+    struct array arr1 = {{2,9,15,21,22}, 10, 5};
+    struct array arr2 = {{1,12,19,20,35}, 10, 5};
+    struct array *arr3;
+    
+    arr3=merge(&arr1, &arr2);
+    display(*arr3);
+    return 0;
+}
