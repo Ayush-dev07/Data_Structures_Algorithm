2748c4070ec8c7ba94386c7a90ab7db68c98a4ee
Ayush Yadav <ayushyadav9812546@gmail.com>
2026-07-21 22:39:27 +0530

    Valid anagram

---
+++ Arrays/LeetCode/Valid_anagram.cpp
@@ -0,0 +1,25 @@
+// Leetcode problem : 242 (Valid anagram)
+
+#include<string>
+#include<stdio.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        int freq[26] = {0};
+        for (char ch : s)
+            freq[ch - 'a']++;
+        for (char ch : t)
+            freq[ch - 'a']--;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0)
+                return false;
+        }
+        return true;
+    }
+};
+
+//Time complexity =O(n)
